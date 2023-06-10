a5eab96b457188f00ddb8afc5ac1182da731dc56
Deepak <lakshmideepakkshatri@gmail.com>
2023-06-10 15:56:50 +0530

    Making the latest post section dynamic completed

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,16 +8,19 @@ import { PostService } from 'src/app/services/post.service';
 })
 export class HomeComponent implements OnInit {
   featuredArrayList: Array<any> = [];
+  latestPostArrayList: Array<any> = [];
 
-  constructor(private postService: PostService) {
-    console.log('In constructor of HomeComponent');
-    this.postService.loadFeatured().subscribe((postData) => {
-      console.log(postData);
-      this.featuredArrayList = postData;
-    });
-  }
+  constructor(private postService: PostService) {}
 
   ngOnInit(): void {
     console.log('In intilization of Home Component');
+
+    this.postService.loadFeatured().subscribe((postData) => {
+      this.featuredArrayList = postData;
+    });
+
+    this.postService.loadLatest().subscribe((latestPostData) => {
+      this.latestPostArrayList = latestPostData;
+    });
   }
 }
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -10,7 +10,45 @@ export class PostService {
 
   loadFeatured() {
     return this.afs
-      .collection('posts', (ref) => ref.where('isFeatured', '==', true))
+      .collection('posts', (ref) =>
+        ref.where('isFeatured', '==', true).limit(4)
+      )
+      .snapshotChanges()
+      .pipe(
+        map((actions: any) => {
+          return actions.map((a: any) => {
+            const data = a.payload.doc.data();
+            const id = a.payload.doc.id;
+            const returnObj = { id, data };
+
+            return returnObj;
+          });
+        })
+      );
+  }
+
+  loadLatest() {
+    return this.afs
+      .collection('posts', (ref) => ref.orderBy('createdAt'))
+      .snapshotChanges()
+      .pipe(
+        map((actions: any) => {
+          return actions.map((a: any) => {
+            const data = a.payload.doc.data();
+            const id = a.payload.doc.id;
+            const returnObj = { id, data };
+
+            return returnObj;
+          });
+        })
+      );
+  }
+
+  loadCategoryPosts(categoryId: string) {
+    return this.afs
+      .collection('posts', (ref) =>
+        ref.where('category.categoryId', '==', categoryId)
+      )
       .snapshotChanges()
       .pipe(
         map((actions: any) => {
