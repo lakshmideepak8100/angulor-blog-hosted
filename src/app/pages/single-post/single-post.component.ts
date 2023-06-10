8f88692c5a8df1558ec3997a2304baec148e4baa
Deepak <lakshmideepakkshatri@gmail.com>
2023-06-10 19:24:27 +0530

    Except for Subscription form the entire project is done.

--- src/app/pages/single-post/single-post.component.ts
+++ src/app/pages/single-post/single-post.component.ts
@@ -1,12 +1,45 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from 'src/app/services/post.service';
+
+import { ActivatedRoute } from '@angular/router';
+import { Post } from 'src/app/modal/post';
 
 @Component({
   selector: 'app-single-post',
   templateUrl: './single-post.component.html',
-  styleUrls: ['./single-post.component.css']
+  styleUrls: ['./single-post.component.css'],
 })
-export class SinglePostComponents implements OnInit {
+export class SinglePostComponent implements OnInit {
+  postContent!: any;
+  similarPostsData: Array<Post> = [];
+  postId!: string;
 
-  
+  constructor(
+    private postService: PostService,
+    private route: ActivatedRoute
+  ) {}
 
+  ngOnInit(): void {
+    this.route.params.subscribe((val) => {
+      this.postId = val['id'];
+      this.updateViewCount();
+      console.log('In Single Post Component');
+      this.postService.loadPost(val['id']).subscribe((content) => {
+        this.postContent = content;
+        console.log(this.postContent);
+
+        this.loadSimilarPosts(this.postContent.category.categoryId);
+      });
+    });
+  }
+
+  loadSimilarPosts(catId: string) {
+    this.postService.loadSimilar(catId).subscribe((val) => {
+      this.similarPostsData = val;
+    });
+  }
+  updateViewCount() {
+    console.log('In update views count');
+    this.postService.countViews(this.postId);
+  }
 }
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { map } from 'rxjs';
+import * as firebase from 'firebase/compat/app';
 
 @Injectable({
   providedIn: 'root',
@@ -63,7 +64,38 @@ export class PostService {
       );
   }
 
-  loadPost(postid) {
-    this.afs.doc(`posts/${postid}`).valueChanges();
+  loadPost(postid: string) {
+    return this.afs.doc(`posts/${postid}`).valueChanges();
+  }
+
+  loadSimilar(catId: string) {
+    return this.afs
+      .collection('posts', (ref) =>
+        ref.where('category.categoryId', '==', catId).limit(6)
+      )
+      .snapshotChanges()
+      .pipe(
+        map((actions: any) => {
+          return actions.map((a: any) => {
+            const data = a.payload.doc.data();
+            const id = a.payload.doc.id;
+            const returnObj = { id, data };
+
+            return returnObj;
+          });
+        })
+      );
+  }
+
+  countViews(postId: string) {
+    const viewsCount = {
+      views: firebase.default.firestore.FieldValue.increment(1),
+    };
+    this.afs
+      .doc(`posts/${postId}`)
+      .update(viewsCount)
+      .then(() => {
+        console.log('Views Value Incremented');
+      });
   }
 }
